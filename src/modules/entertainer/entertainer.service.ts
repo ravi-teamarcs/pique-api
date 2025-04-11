9b1d3404254fd2b94d6ab19f8e08514bdf105b2c
Bhawani Thakur <bhawani.thakur@teamarcs.com>
2025-04-11 11:07:48 +0530

    get all status of booking

--- src/modules/entertainer/entertainer.service.ts
+++ src/modules/entertainer/entertainer.service.ts
@@ -327,6 +327,7 @@ export class EntertainerService {
             'cancelled',
             'completed',
             'accepted',
+            'rejected'
           ],
         })
         .select([
