80eadd5b37f3509e7eb279688dc00c4a35aa1f0c
Bhawani Thakur <bhawani.thakur@teamarcs.com>
2025-05-05 12:16:59 +0530

    chat

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-// import { ChatGateway } from './chat.gateway';
+import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
-import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Message])],
