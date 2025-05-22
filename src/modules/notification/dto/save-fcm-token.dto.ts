c72f2d8b36bf05a9b911695be665e3a77ab933d2
Bhawani Thakur <bhawani.thakur@teamarcs.com>
2025-05-22 22:46:24 +0530

    feat:Notification module changes

--- src/modules/notification/dto/save-fcm-token.dto.ts
+++ src/modules/notification/dto/save-fcm-token.dto.ts
@@ -2,12 +2,10 @@ import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
 
 export class SaveFcmTokenDto {
   @IsNumber()
+  @IsNotEmpty()
   userId: number;
 
   @IsString()
   @IsNotEmpty()
   token: string;
-
-  @IsString()
-  userAgent: string;
 }
