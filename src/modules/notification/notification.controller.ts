6e77d16ec47d460f5b1627a994231fa11e013a37
Bhawani Thakur <bhawani.thakur@teamarcs.com>
2025-06-11 17:15:01 +0530

    fixed Api marked all as read

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -72,6 +72,7 @@ export class NotificationController {
   }
 
   @Patch('mark-all-read')
+  @UseGuards(JwtAuthGuard)
   async markAllAsRead(@Request() req) {
     const { userId } = req.user;
     return this.notificationService.markAllAsRead(userId);
