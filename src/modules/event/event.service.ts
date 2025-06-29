e24e19726651cbbe646b9c2ba7a138caad6c75fe
Bhawani Thakur <bhawani.thakur@teamarcs.com>
2025-06-29 23:13:22 +0530

    changes in event slug formation

--- src/modules/event/event.service.ts
+++ src/modules/event/event.service.ts
@@ -404,8 +404,9 @@ export class EventService {
     const neighbourhoodNameString = neighbourhoodName
       ? `${neighbourhoodName}/`
       : '';
+    const stateString = stateCode ? `(, ${stateCode})` : '';
 
-    const slug = `${formattedDate} at ${timeUTC} ${titleString} at ${neighbourhoodNameString}${name} in ${city ?? ''}, ${stateCode ?? ''}`;
+    const slug = `${formattedDate} at ${timeUTC} ${titleString} at ${neighbourhoodNameString}${name} in ${city ?? ''} ${stateString}`;
 
     return slug;
   }
