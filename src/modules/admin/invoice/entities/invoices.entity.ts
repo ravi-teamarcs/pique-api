25376df5ea58d80bec82dfbf4d3cc90d0e0e4ef1
Bhawani Thakur <bhawani.thakur@teamarcs.com>
2025-07-16 16:25:09 +0530

    fixed : invoiceing

--- src/modules/admin/invoice/entities/invoices.entity.ts
+++ src/modules/admin/invoice/entities/invoices.entity.ts
@@ -44,7 +44,7 @@ export class Invoice {
   @Column({ type: 'decimal' })
   total_amount: number;
 
-  @Column({ type: 'decimal', precision: 5, scale: 2 })
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
   tax_rate: number;
 
   @Column({ type: 'decimal' })
